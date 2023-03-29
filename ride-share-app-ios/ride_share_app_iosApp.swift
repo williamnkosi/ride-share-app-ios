9076564fbd61182f2db0378b5c04f84493388e86
William Nkosi <william.dev.acc@gmail.com>
2023-03-29 13:22:46 -0400

    Initial Commit

---
+++ ride-share-app-ios/ride_share_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ride_share_app_iosApp.swift
+//  ride-share-app-ios
+//
+//  Created by William Nkosi on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ride_share_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
